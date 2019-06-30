3ba8d6c5410d6161ce2b3d7c77f7a1d518710386
Miguel <magudob@gmail.com>
2019-06-30 14:33:20 +0200

    server

--- server/timeLog.php
+++ server/timeLog.php
@@ -1,8 +1,18 @@
  <?php
-    $newTime = $_POST['time'];
-    
+    //$newTime = $_POST['time'];
+	//$newTime = $_REQUEST;
     $myfile = fopen("times.txt", "a") or die("Unable to open file!");
-    fwrite($myfile, "\n". $newTime);
-    fclose($myfile);
+    //fwrite($myfile, "\n". $newTime);
+	//fwrite($myfile, "\n". var_export($_REQUEST, true));
+    //fclose($myfile);
+	
+	foreach($_REQUEST as $fieldName=>$fieldVal){
+		$container = explode("=",$fieldName);
+		if($container[0] == "time"){
+			fwrite($myfile, $fieldVal. "\n");
+		}
+	}
+	
+	fclose($myfile);
 ?>
  
